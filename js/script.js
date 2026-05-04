7ae5b7af94e3defa2f25b68238a5652bde662933
student <student@itismeucci.com>
2026-05-04 13:18:16 +0200

    creazione progetto

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const bischero = document.querySelector("#bischero");
+
+
+document.querySelector("#btn").addEventListener("click", () => {
+  bischero.innerHTML = "sei un po' un bischero non tu pensi?";
+});
